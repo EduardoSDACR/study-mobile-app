53d4c93a34cfb3ee46652d478d6ca972388b1c32
Eduardo <eduardo_sdacr@hotmail.com>
2022-10-17 17:26:58 -0500

    Primera pantalla

---
+++ android/app/src/main/kotlin/com/example/studyng_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studyng_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:studyng_app/data_uploader_screen.dart';
+import 'package:studyng_app/firebase_options.dart';
+import 'package:get/get.dart';
+import 'package:studyng_app/screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+/* Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    GetMaterialApp(
+      home: DataUploaderScreen(),
+    ),
+  );
+} */
